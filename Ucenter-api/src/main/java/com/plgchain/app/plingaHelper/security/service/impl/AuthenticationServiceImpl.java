cad601dcd49755ca2cc5286e7602345bc8ba0563
git <eae966@gmail.com>
2023-06-15 14:22:06 +0330

    erterterert

--- Ucenter-api/src/main/java/com/plgchain/app/plingaHelper/security/service/impl/AuthenticationServiceImpl.java
+++ Ucenter-api/src/main/java/com/plgchain/app/plingaHelper/security/service/impl/AuthenticationServiceImpl.java
@@ -21,34 +21,40 @@ import com.plgchain.app.plingaHelper.service.UserService;
 @Service
 @RequiredArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
-    private final UserService userService;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtService jwtService;
-    private final AuthenticationManager authenticationManager;
+	private final UserService userService;
+	private final PasswordEncoder passwordEncoder;
+	private final JwtService jwtService;
+	private final AuthenticationManager authenticationManager;
 
-    private final static Logger logger = LoggerFactory.getLogger(AuthenticationServiceImpl.class);
-    @Override
-    public JwtAuthenticationResponse signup(SignUpRequest request) {
-        var user = User.builder().firstname(request.getFirstName()).lastname(request.getLastName())
-                .emailAddress(request.getEmail()).password(passwordEncoder.encode(request.getPassword()))
-                //.role(Role.USER)
-                .build();
-        userService.save(user);
-        var jwt = jwtService.generateToken(user);
-        return JwtAuthenticationResponse.builder().token(jwt).build();
-    }
+	private final static Logger logger = LoggerFactory.getLogger(AuthenticationServiceImpl.class);
 
-    @Override
-    public JwtAuthenticationResponse signin(SigninRequest request) {
-    	logger.info("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
-        logger.info("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
-        var user = userService.findByEmailAddress(request.getEmail())
-                .orElseThrow(() -> new IllegalArgumentException("Invalid email or password."));
-        logger.info("cccccccccccccccccccccccccccccccccccccccccccccccccc");
-        var jwt = jwtService.generateToken(user);
-        logger.info("dddddddddddddddddddddddddddddddddddddddddddddddddddd");
-        return JwtAuthenticationResponse.builder().token(jwt).build();
-    }
+	@Override
+	public JwtAuthenticationResponse signup(SignUpRequest request) {
+		var user = User.builder().firstname(request.getFirstName()).lastname(request.getLastName())
+				.emailAddress(request.getEmail()).password(passwordEncoder.encode(request.getPassword()))
+				// .role(Role.USER)
+				.build();
+		userService.save(user);
+		var jwt = jwtService.generateToken(user);
+		return JwtAuthenticationResponse.builder().token(jwt).build();
+	}
+
+	@Override
+	public JwtAuthenticationResponse signin(SigninRequest request) {
+		try {
+			logger.info("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+			authenticationManager
+					.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+			logger.info("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+			var user = userService.findByEmailAddress(request.getEmail())
+					.orElseThrow(() -> new IllegalArgumentException("Invalid email or password."));
+			logger.info("cccccccccccccccccccccccccccccccccccccccccccccccccc");
+			var jwt = jwtService.generateToken(user);
+			logger.info("dddddddddddddddddddddddddddddddddddddddddddddddddddd");
+			return JwtAuthenticationResponse.builder().token(jwt).build();
+		} catch (Exception e) {
+			logger.warn("Errrrrrrror is " + e.getMessage());
+		}
+		return null;
+	}
 }
