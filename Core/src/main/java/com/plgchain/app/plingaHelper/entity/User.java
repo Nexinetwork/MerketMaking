4f8c747e36abb8654cd4d9d106cc887e7bd40a04
git <eae966@gmail.com>
2023-06-15 14:26:25 +0330

    345345345

--- Core/src/main/java/com/plgchain/app/plingaHelper/entity/User.java
+++ Core/src/main/java/com/plgchain/app/plingaHelper/entity/User.java
@@ -102,7 +102,7 @@ public class User implements UserDetails,Serializable {
 	@Override
 	public boolean isAccountNonExpired() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
@@ -114,7 +114,7 @@ public class User implements UserDetails,Serializable {
 	@Override
 	public boolean isCredentialsNonExpired() {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
