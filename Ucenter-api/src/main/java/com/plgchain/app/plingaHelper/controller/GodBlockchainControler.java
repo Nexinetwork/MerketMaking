97c344dfcd71f9d9317c8b5c508b8059dcd93b32
git <eae966@gmail.com>
2023-06-15 20:53:17 +0330

    sdfsdfsdsdf

--- Ucenter-api/src/main/java/com/plgchain/app/plingaHelper/controller/GodBlockchainControler.java
+++ Ucenter-api/src/main/java/com/plgchain/app/plingaHelper/controller/GodBlockchainControler.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class GodBlockchainControler extends BaseController implements Serializab
 
 	private final static Logger logger = LoggerFactory.getLogger(GodBlockchainControler.class);
 
-	@RequestMapping("/ping")
+	@GetMapping("/ping")
 	public MessageResult ping() {
 		try {
 			return success("Pong God");
