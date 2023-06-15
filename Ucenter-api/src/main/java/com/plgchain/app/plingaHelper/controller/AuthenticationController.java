4b77381d9b26f3b3d1f02363ddc0d08fb90f66f2
git <eae966@gmail.com>
2023-06-15 14:08:47 +0330

    rtyrtyrtyrt

--- Ucenter-api/src/main/java/com/plgchain/app/plingaHelper/controller/AuthenticationController.java
+++ Ucenter-api/src/main/java/com/plgchain/app/plingaHelper/controller/AuthenticationController.java
@@ -15,6 +15,8 @@ import com.plgchain.app.plingaHelper.security.dao.request.SignUpRequest;
 import com.plgchain.app.plingaHelper.security.dao.request.SigninRequest;
 import com.plgchain.app.plingaHelper.security.dao.response.JwtAuthenticationResponse;
 import com.plgchain.app.plingaHelper.security.service.AuthenticationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import lombok.RequiredArgsConstructor;
 
@@ -28,15 +30,21 @@ public class AuthenticationController implements Serializable {
 
 	private static final long serialVersionUID = -1153034969946962877L;
 
+	private final static Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
+
 
 	private final AuthenticationService authenticationService;
     @PostMapping("/signup")
     public ResponseEntity<JwtAuthenticationResponse> signup(@RequestBody SignUpRequest request) {
+    	logger.info("aaaaaaaaaaaa" + request.toString());
         return ResponseEntity.ok(authenticationService.signup(request));
     }
 
     @PostMapping("/signin")
     public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SigninRequest request) {
+    	logger.info("bbbbbbbbbbbbbbbbbbbbbb" + request.toString());
+    	JwtAuthenticationResponse res = authenticationService.signin(request);
+    	logger.info("res is" + res);
         return ResponseEntity.ok(authenticationService.signin(request));
     }
 
