abfebd7c24587343614041fe798fbb42052d4843
git <eae966@gmail.com>
2023-06-15 14:30:55 +0330

    ertertertert

--- Ucenter-api/src/main/java/com/plgchain/app/plingaHelper/security/service/impl/AuthenticationServiceImpl.java
+++ Ucenter-api/src/main/java/com/plgchain/app/plingaHelper/security/service/impl/AuthenticationServiceImpl.java
@@ -1,15 +1,11 @@
 package com.plgchain.app.plingaHelper.security.service.impl;
 
 import lombok.RequiredArgsConstructor;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.plgchain.app.plingaHelper.controller.AuthenticationController;
 import com.plgchain.app.plingaHelper.entity.User;
 import com.plgchain.app.plingaHelper.security.dao.request.SignUpRequest;
 import com.plgchain.app.plingaHelper.security.dao.request.SigninRequest;
@@ -21,40 +17,28 @@ import com.plgchain.app.plingaHelper.service.UserService;
 @Service
 @RequiredArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
-	private final UserService userService;
-	private final PasswordEncoder passwordEncoder;
-	private final JwtService jwtService;
-	private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    @Override
+    public JwtAuthenticationResponse signup(SignUpRequest request) {
+        var user = User.builder().firstname(request.getFirstName()).lastname(request.getLastName())
+                .emailAddress(request.getEmail()).password(passwordEncoder.encode(request.getPassword()))
+                //.role(Role.USER)
+                .build();
+        userService.save(user);
+        var jwt = jwtService.generateToken(user);
+        return JwtAuthenticationResponse.builder().token(jwt).build();
+    }
 
-	private final static Logger logger = LoggerFactory.getLogger(AuthenticationServiceImpl.class);
-
-	@Override
-	public JwtAuthenticationResponse signup(SignUpRequest request) {
-		var user = User.builder().firstname(request.getFirstName()).lastname(request.getLastName())
-				.emailAddress(request.getEmail()).password(passwordEncoder.encode(request.getPassword()))
-				// .role(Role.USER)
-				.build();
-		userService.save(user);
-		var jwt = jwtService.generateToken(user);
-		return JwtAuthenticationResponse.builder().token(jwt).build();
-	}
-
-	@Override
-	public JwtAuthenticationResponse signin(SigninRequest request) {
-		try {
-			logger.info("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-			authenticationManager
-					.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
-			logger.info("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
-			var user = userService.findByEmailAddress(request.getEmail())
-					.orElseThrow(() -> new IllegalArgumentException("Invalid email or password."));
-			logger.info("cccccccccccccccccccccccccccccccccccccccccccccccccc");
-			var jwt = jwtService.generateToken(user);
-			logger.info("dddddddddddddddddddddddddddddddddddddddddddddddddddd");
-			return JwtAuthenticationResponse.builder().token(jwt).build();
-		} catch (Exception e) {
-			logger.warn("Errrrrrrror is " + e.getMessage());
-		}
-		return null;
-	}
+    @Override
+    public JwtAuthenticationResponse signin(SigninRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        var user = userService.findByEmailAddress(request.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid email or password."));
+        var jwt = jwtService.generateToken(user);
+        return JwtAuthenticationResponse.builder().token(jwt).build();
+    }
 }
