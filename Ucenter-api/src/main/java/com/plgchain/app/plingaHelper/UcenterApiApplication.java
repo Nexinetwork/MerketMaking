2d71f6e25f08bb367da31a958bd4a651526eddb6
git <eae966@gmail.com>
2023-06-16 12:45:15 +0330

    BlockchainHealthBusiness has been fixed.

--- Ucenter-api/src/main/java/com/plgchain/app/plingaHelper/UcenterApiApplication.java
+++ Ucenter-api/src/main/java/com/plgchain/app/plingaHelper/UcenterApiApplication.java
@@ -1,4 +1,4 @@
-package com.plgchain.app;
+package com.plgchain.app.plingaHelper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
