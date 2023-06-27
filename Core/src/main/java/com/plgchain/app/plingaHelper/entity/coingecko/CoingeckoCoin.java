d5ebdd8616e788be70b7af2f77411200acc86aff
git <eae966@gmail.com>
2023-06-27 22:31:16 +0330

    ewrwerwe

--- Core/src/main/java/com/plgchain/app/plingaHelper/entity/coingecko/CoingeckoCoin.java
+++ Core/src/main/java/com/plgchain/app/plingaHelper/entity/coingecko/CoingeckoCoin.java
@@ -106,97 +106,78 @@ public class CoingeckoCoin implements Serializable {
 	public CoingeckoCoin(JSONObject jo) {
 		this.id = jo.getString("id");
 		this.symbol = jo.getString("symbol");
-	    this.name = jo.getString("symbol");
-	    this.mainPlatform = jo.getString("asset_platform_id");
-	    this.platforms = jo.getJSONObject("platforms");
-	    this.detail_platforms = jo.getJSONObject("detail_platforms");
-	    this.market_data = jo.getJSONObject("market_data");
-	    this.current_price = jo.getJSONObject("market_data").getJSONObject("current_price");
-	    this.market_cap = jo.getJSONObject("market_data").getJSONObject("market_cap");
-	    this.market_cap_rank = jo.getInteger("market_cap_rank");
-	    this.total_supply = jo.getJSONObject("market_data").getBigDecimal("total_supply");
-	    this.max_supply = jo.getJSONObject("market_data").getBigDecimal("max_supply");
-	    this.priceInUsd = jo.getJSONObject("market_data").getJSONObject("current_price").getBigDecimal("usd");
-	    this.image = jo.getJSONObject("image");
-	    this.total_volume = jo.getJSONObject("market_data").getJSONObject("total_volume");
-	    this.high_24h = jo.getJSONObject("market_data").getJSONObject("high_24h");
-	    this.high24HInUsd = jo.getJSONObject("market_data").getJSONObject("high_24h").getBigDecimal("usd");
-	    this.low_24h = jo.getJSONObject("market_data").getJSONObject("low_24h");
-	    this.low24HInUsd = jo.getJSONObject("market_data").getJSONObject("low_24h").getBigDecimal("usd");
-	    this.price_change_24h = jo.getJSONObject("market_data").getBigDecimal("price_change_24h");
-	    this.price_change_percentage_24h = jo.getJSONObject("market_data").getBigDecimal("price_change_percentage_24h");
-	    this.market_cap_change_24h = jo.getJSONObject("market_data").getBigDecimal("market_cap_change_24h");
-	    this.market_cap_change_percentage_24h = jo.getJSONObject("market_data").getBigDecimal("market_cap_change_percentage_24h");
-	    this.mainContractAddress = jo.getString("contract_address");
+		this.name = jo.getString("symbol");
+		this.mainPlatform = jo.getString("asset_platform_id");
+		this.platforms = jo.getJSONObject("platforms");
+		this.detail_platforms = jo.getJSONObject("detail_platforms");
+		this.market_data = jo.getJSONObject("market_data");
+		if (jo.getJSONObject("market_data").getJSONObject("current_price") != null)
+			this.current_price = jo.getJSONObject("market_data").getJSONObject("current_price");
+		if (jo.getJSONObject("market_data").getJSONObject("market_cap") != null)
+			this.market_cap = jo.getJSONObject("market_data").getJSONObject("market_cap");
+		this.market_cap_rank = jo.getInteger("market_cap_rank");
+		if (jo.getJSONObject("market_data").getBigDecimal("total_supply") != null)
+			this.total_supply = jo.getJSONObject("market_data").getBigDecimal("total_supply");
+		this.max_supply = jo.getJSONObject("market_data").getBigDecimal("max_supply");
+		this.priceInUsd = jo.getJSONObject("market_data").getJSONObject("current_price").getBigDecimal("usd");
+		this.image = jo.getJSONObject("image");
+		this.total_volume = jo.getJSONObject("market_data").getJSONObject("total_volume");
+		this.high_24h = jo.getJSONObject("market_data").getJSONObject("high_24h");
+		this.high24HInUsd = jo.getJSONObject("market_data").getJSONObject("high_24h").getBigDecimal("usd");
+		this.low_24h = jo.getJSONObject("market_data").getJSONObject("low_24h");
+		this.low24HInUsd = jo.getJSONObject("market_data").getJSONObject("low_24h").getBigDecimal("usd");
+		this.price_change_24h = jo.getJSONObject("market_data").getBigDecimal("price_change_24h");
+		this.price_change_percentage_24h = jo.getJSONObject("market_data").getBigDecimal("price_change_percentage_24h");
+		this.market_cap_change_24h = jo.getJSONObject("market_data").getBigDecimal("market_cap_change_24h");
+		this.market_cap_change_percentage_24h = jo.getJSONObject("market_data")
+				.getBigDecimal("market_cap_change_percentage_24h");
+		this.mainContractAddress = jo.getString("contract_address");
 	}
 
 	public CoingeckoCoin(CoingeckoCoinAssetDto dto) {
-	    this.id = dto.getId();
-	    this.symbol = dto.getSymbol();
-	    this.name = dto.getName();
-	    this.mainPlatform = dto.getMainPlatform();
-	    this.platforms = dto.getPlatforms();
-	    this.detail_platforms = dto.getDetail_platforms();
-	    this.market_data = dto.getMarket_data();
-	    this.current_price = dto.getCurrent_price();
-	    this.market_cap = dto.getMarket_cap();
-	    this.market_cap_rank = dto.getMarket_cap_rank();
-	    this.total_supply = dto.getTotal_supply();
-	    this.max_supply = dto.getMax_supply();
-	    this.priceInUsd = dto.getPriceInUsd();
-	    this.image = dto.getImage();
-	    this.total_volume = dto.getTotal_volume();
-	    this.high_24h = dto.getHigh_24h();
-	    this.high24HInUsd = dto.getHigh24HInUsd();
-	    this.low_24h = dto.getLow_24h();
-	    this.low24HInUsd = dto.getLow24HInUsd();
-	    this.price_change_24h = dto.getPrice_change_24h();
-	    this.price_change_percentage_24h = dto.getPrice_change_percentage_24h();
-	    this.market_cap_change_24h = dto.getMarket_cap_change_24h();
-	    this.market_cap_change_percentage_24h = dto.getMarket_cap_change_percentage_24h();
-	    this.mainContractAddress = dto.getMainContractAddress();
-	    this.originalJson = dto.getOriginalJson();
-	    this.editedJson = dto.getEditedJson();
-	    this.creationDate = dto.getCreationDate();
-	    this.lastUpdateDate = dto.getLastUpdateDate();
-	    this.lastCheck = dto.getLastCheck();
+		this.id = dto.getId();
+		this.symbol = dto.getSymbol();
+		this.name = dto.getName();
+		this.mainPlatform = dto.getMainPlatform();
+		this.platforms = dto.getPlatforms();
+		this.detail_platforms = dto.getDetail_platforms();
+		this.market_data = dto.getMarket_data();
+		this.current_price = dto.getCurrent_price();
+		this.market_cap = dto.getMarket_cap();
+		this.market_cap_rank = dto.getMarket_cap_rank();
+		this.total_supply = dto.getTotal_supply();
+		this.max_supply = dto.getMax_supply();
+		this.priceInUsd = dto.getPriceInUsd();
+		this.image = dto.getImage();
+		this.total_volume = dto.getTotal_volume();
+		this.high_24h = dto.getHigh_24h();
+		this.high24HInUsd = dto.getHigh24HInUsd();
+		this.low_24h = dto.getLow_24h();
+		this.low24HInUsd = dto.getLow24HInUsd();
+		this.price_change_24h = dto.getPrice_change_24h();
+		this.price_change_percentage_24h = dto.getPrice_change_percentage_24h();
+		this.market_cap_change_24h = dto.getMarket_cap_change_24h();
+		this.market_cap_change_percentage_24h = dto.getMarket_cap_change_percentage_24h();
+		this.mainContractAddress = dto.getMainContractAddress();
+		this.originalJson = dto.getOriginalJson();
+		this.editedJson = dto.getEditedJson();
+		this.creationDate = dto.getCreationDate();
+		this.lastUpdateDate = dto.getLastUpdateDate();
+		this.lastCheck = dto.getLastCheck();
 	}
-
 
 	public CoingeckoCoinAssetDto getAsDto() {
-	    return CoingeckoCoinAssetDto.builder()
-	            .id(id)
-	            .symbol(symbol)
-	            .name(name)
-	            .mainPlatform(mainPlatform)
-	            .platforms(platforms)
-	            .detail_platforms(detail_platforms)
-	            .market_data(market_data)
-	            .current_price(current_price)
-	            .market_cap(market_cap)
-	            .market_cap_rank(market_cap_rank)
-	            .total_supply(total_supply)
-	            .max_supply(max_supply)
-	            .priceInUsd(priceInUsd)
-	            .image(image)
-	            .total_volume(total_volume)
-	            .high_24h(high_24h)
-	            .high24HInUsd(high24HInUsd)
-	            .low_24h(low_24h)
-	            .low24HInUsd(low24HInUsd)
-	            .price_change_24h(price_change_24h)
-	            .price_change_percentage_24h(price_change_percentage_24h)
-	            .market_cap_change_24h(market_cap_change_24h)
-	            .market_cap_change_percentage_24h(market_cap_change_percentage_24h)
-	            .mainContractAddress(mainContractAddress)
-	            .originalJson(originalJson)
-	            .editedJson(editedJson)
-	            .creationDate(creationDate)
-	            .lastUpdateDate(lastUpdateDate)
-	            .lastCheck(lastCheck)
-	            .build();
+		return CoingeckoCoinAssetDto.builder().id(id).symbol(symbol).name(name).mainPlatform(mainPlatform)
+				.platforms(platforms).detail_platforms(detail_platforms).market_data(market_data)
+				.current_price(current_price).market_cap(market_cap).market_cap_rank(market_cap_rank)
+				.total_supply(total_supply).max_supply(max_supply).priceInUsd(priceInUsd).image(image)
+				.total_volume(total_volume).high_24h(high_24h).high24HInUsd(high24HInUsd).low_24h(low_24h)
+				.low24HInUsd(low24HInUsd).price_change_24h(price_change_24h)
+				.price_change_percentage_24h(price_change_percentage_24h).market_cap_change_24h(market_cap_change_24h)
+				.market_cap_change_percentage_24h(market_cap_change_percentage_24h)
+				.mainContractAddress(mainContractAddress).originalJson(originalJson).editedJson(editedJson)
+				.creationDate(creationDate).lastUpdateDate(lastUpdateDate).lastCheck(lastCheck).build();
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -208,6 +189,5 @@ public class CoingeckoCoin implements Serializable {
 		return Objects.equals(editedJson, other.editedJson) && id == other.id
 				&& Objects.equals(originalJson, other.originalJson);
 	}
-
 
 }
