5fb9cd3bf85bb388eb71dacd4cc9d7ad31bd008a
git <eae966@gmail.com>
2023-07-05 13:22:00 +0330

    Some Updates,

--- Ucenter-api/src/main/java/com/plgchain/app/plingaHelper/controller/godController/BlockchainControler.java
+++ Ucenter-api/src/main/java/com/plgchain/app/plingaHelper/controller/godController/BlockchainControler.java
@@ -5,19 +5,25 @@ import java.io.Serializable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson2.JSON;
 import com.plgchain.app.plingaHelper.bean.BlockchainBean;
+import com.plgchain.app.plingaHelper.constant.AdminCommandType;
+import com.plgchain.app.plingaHelper.constant.SysConstant;
 import com.plgchain.app.plingaHelper.controller.BaseController;
 import com.plgchain.app.plingaHelper.dto.BlockchainNodeDto;
 import com.plgchain.app.plingaHelper.entity.Blockchain;
 import com.plgchain.app.plingaHelper.entity.BlockchainNode;
 import com.plgchain.app.plingaHelper.exception.RestActionError;
+import com.plgchain.app.plingaHelper.type.CommandToRun;
 import com.plgchain.app.plingaHelper.util.MessageResult;
 
+import jakarta.inject.Inject;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -29,8 +35,11 @@ public class BlockchainControler extends BaseController implements Serializable 
 
 	private final static Logger logger = LoggerFactory.getLogger(BlockchainControler.class);
 
-	@Autowired
+	@Inject
 	private BlockchainBean blockchainBean;
+
+	@Inject
+	private KafkaTemplate<String, String> kafkaTemplate;
 
 	@RequestMapping("/ping")
 	public MessageResult ping() {
@@ -61,6 +70,9 @@ public class BlockchainControler extends BaseController implements Serializable 
 	public MessageResult createNewBlockchainNode(@RequestBody BlockchainNodeDto node) {
 		try {
 			var result = blockchainBean.createBlockchainNode(node);
+			CommandToRun ctr = new CommandToRun();
+			ctr.setAdminCommandType(AdminCommandType.UPDATEBLOCKCHAIN);
+			kafkaTemplate.send(SysConstant.KAFKA_ADMIN_COMMAND, JSON.toJSONString(ctr));
 			return success(String.format("Blockchain node %s has been created.", result.getAsDto()));
 		} catch (RestActionError e) {
 			// TODO Auto-generated catch block
