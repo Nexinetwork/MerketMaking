8699dd8734912990b82dc91b52053018697af79d
git <eae966@gmail.com>
2023-07-10 17:17:48 +0330

    Some Updates.

--- Ucenter-api/src/main/java/com/plgchain/app/plingaHelper/controller/godController/CoinControler.java
+++ Ucenter-api/src/main/java/com/plgchain/app/plingaHelper/controller/godController/CoinControler.java
@@ -10,18 +10,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.fastjson2.JSON;
-import com.netflix.servo.util.Strings;
 import com.plgchain.app.plingaHelper.bean.BlockchainBean;
-import com.plgchain.app.plingaHelper.constant.AdminCommandType;
-import com.plgchain.app.plingaHelper.constant.SysConstant;
 import com.plgchain.app.plingaHelper.controller.BaseController;
-import com.plgchain.app.plingaHelper.dto.BlockchainNodeDto;
-import com.plgchain.app.plingaHelper.entity.Blockchain;
 import com.plgchain.app.plingaHelper.exception.RestActionError;
 import com.plgchain.app.plingaHelper.service.CoinService;
 import com.plgchain.app.plingaHelper.service.SmartContractService;
-import com.plgchain.app.plingaHelper.type.CommandToRun;
 import com.plgchain.app.plingaHelper.type.request.CoinReq;
 import com.plgchain.app.plingaHelper.util.MessageResult;
 
@@ -56,10 +49,10 @@ public class CoinControler extends BaseController implements Serializable {
 			return success(String.format("Coin %s has been created.", coin));
 		} catch (RestActionError e) {
 			// TODO Auto-generated catch block
-			error(e.getMessage());
+			logger.error(e.getMessage());
 			return error(e.getMessage());
 		} catch (Exception e) {
-			e.printStackTrace();
+			logger.error(e.getMessage());
 			return error(e.getMessage());
 		}
 	}
