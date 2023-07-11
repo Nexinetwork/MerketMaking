ce0dae12c3400d13b74ee9746d292e7f7bfabb48
git <eae966@gmail.com>
2023-07-11 15:25:19 +0330

    Bug Fixes.

--- Transfer/src/main/java/com/plgchain/app/plingaHelper/schedule/GenerateTankhahWalletSchedule.java
+++ Transfer/src/main/java/com/plgchain/app/plingaHelper/schedule/GenerateTankhahWalletSchedule.java
@@ -19,40 +19,40 @@ import java.util.List;
 @Component
 public class GenerateTankhahWalletSchedule implements Serializable {
 
-    private static final long serialVersionUID = 7504602259969637312L;
-    private static final Logger logger = LoggerFactory.getLogger(GenerateTankhahWalletSchedule.class);
+	private static final long serialVersionUID = 7504602259969637312L;
+	private static final Logger logger = LoggerFactory.getLogger(GenerateTankhahWalletSchedule.class);
 
-    private final InitBean initBean;
-    private final TankhahWalletService tankhahWalletService;
+	private final InitBean initBean;
+	private final TankhahWalletService tankhahWalletService;
 
-    @Inject
-    public GenerateTankhahWalletSchedule(InitBean initBean, TankhahWalletService tankhahWalletService) {
-        this.initBean = initBean;
-        this.tankhahWalletService = tankhahWalletService;
-    }
+	@Inject
+	public GenerateTankhahWalletSchedule(InitBean initBean, TankhahWalletService tankhahWalletService) {
+		this.initBean = initBean;
+		this.tankhahWalletService = tankhahWalletService;
+	}
 
-    @Scheduled(cron = "0 */10 * * * *", zone = "GMT")
-    public void generateTankhahWallet() {
-        if (!initBean.doesActionRunning("generateTankhahWallet")) {
-            initBean.startActionRunning("generateTankhahWallet");
+	@Scheduled(cron = "0 */10 * * * *", zone = "GMT")
+	public void generateTankhahWallet() {
+		if (!initBean.doesActionRunning("generateTankhahWallet")) {
+			initBean.startActionRunning("generateTankhahWallet");
 
-            List<SmartContract> smartContracts = tankhahWalletService.findAllSmartContractsNotHaveTankhahWallet();
-            smartContracts.forEach(smartContract -> {
-                EvmWalletDto w = EvmWalletUtil.generateRandomWallet();
-                TankhahWallet tankhahWallet = TankhahWallet.builder()
-                        .balance(w.getBalance())
-                        .contract(smartContract)
-                        .privateKey(w.getPrivateKey())
-                        .tankhahWalletType(TankhahWalletType.TRANSFER)
-                        .publicKey(null)
-                        .build();
-                tankhahWallet = tankhahWalletService.save(tankhahWallet);
-                logger.info(String.format("Tankhah wallet %s for transfer has been created.", tankhahWallet));
-            });
+			List<SmartContract> smartContracts = tankhahWalletService.findAllSmartContractsNotHaveTankhahWallet();
+			smartContracts.forEach(smartContract -> {
+				try {
+					EvmWalletDto w = EvmWalletUtil.generateRandomWallet();
+					TankhahWallet tankhahWallet = TankhahWallet.builder().balance(w.getBalance())
+							.contract(smartContract).privateKey(w.getPrivateKey())
+							.tankhahWalletType(TankhahWalletType.TRANSFER).publicKey(null).build();
+					tankhahWallet = tankhahWalletService.save(tankhahWallet);
+					logger.info(String.format("Tankhah wallet %s for transfer has been created.", tankhahWallet));
+				} catch (Exception e) {
+					logger.error(e.getMessage());
+				}
+			});
 
-            initBean.stopActionRunning("generateTankhahWallet");
-        } else {
-            logger.warn("Schedule method generateTankhahWallet already running, skipping it.");
-        }
-    }
+			initBean.stopActionRunning("generateTankhahWallet");
+		} else {
+			logger.warn("Schedule method generateTankhahWallet already running, skipping it.");
+		}
+	}
 }
