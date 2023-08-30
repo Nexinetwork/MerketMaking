f5758fa7e9d2c2324bda0e1ed0d724c86f64c534
git <eae966@gmail.com>
2023-08-30 18:23:22 +0330

    Some Updates.

---
+++ DefiMarketMaking/src/main/java/com/plgchain/app/plingaHelper/consumer/AdminCommandConsumer.java
@@ -0,0 +1,53 @@
+/**
+ *
+ */
+package com.plgchain.app.plingaHelper.consumer;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import com.alibaba.fastjson2.JSON;
+import com.plgchain.app.plingaHelper.bean.InitBean;
+import com.plgchain.app.plingaHelper.constant.AdminCommandType;
+import com.plgchain.app.plingaHelper.constant.SysConstant;
+import com.plgchain.app.plingaHelper.type.CommandToRun;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ *
+ */
+@Component
+@Slf4j
+public class AdminCommandConsumer implements Serializable {
+
+	private static final long serialVersionUID = 8556636738432380764L;
+	private final static Logger logger = LoggerFactory.getLogger(AdminCommandConsumer.class);
+
+	@Autowired
+	private InitBean initBean;
+
+	@KafkaListener(topics = SysConstant.KAFKA_ADMIN_COMMAND, containerFactory = "kafkaListenerContainerFactory", groupId = "DefiMarketMakingService.handleAdminCommand")
+	public void handleAdminCommand(List<ConsumerRecord<String, String>> records) {
+		try {
+			for (int i = 0; i < records.size(); i++) {
+				ConsumerRecord<String, String> record = records.get(i);
+				logger.info("New message is : " + record.value());
+				CommandToRun ctr = JSON.parseObject(record.value(), CommandToRun.class);
+
+			}
+		} catch (Exception e) {
+			logger.info(e.getMessage());
+			e.printStackTrace();
+		}
+
+	}
+
+}
