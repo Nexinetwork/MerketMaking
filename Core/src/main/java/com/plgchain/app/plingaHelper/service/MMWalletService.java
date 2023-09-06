52d9a382d300da7dea8975dc5cfb6bb4c8eb5174
git <eae966@gmail.com>
2023-09-07 00:25:31 +0330

    Some Improvements.

--- Core/src/main/java/com/plgchain/app/plingaHelper/service/MMWalletService.java
+++ Core/src/main/java/com/plgchain/app/plingaHelper/service/MMWalletService.java
@@ -8,6 +8,7 @@ import java.util.stream.IntStream;
 
 import org.springframework.stereotype.Service;
 
+import com.plgchain.app.plingaHelper.constant.WalletType;
 import com.plgchain.app.plingaHelper.dto.MarketMakingWalletDto;
 import com.plgchain.app.plingaHelper.entity.MMWallet;
 import com.plgchain.app.plingaHelper.microService.MMWalletMicroService;
@@ -33,19 +34,37 @@ public class MMWalletService {
 		return mmWalletMicroService.findByContractId(contractId);
 	}
 
-	public MMWallet setTransferWallet(MMWallet mmw, List<MarketMakingWalletDto> mmwdNewLst,
-			String secretKey) {
+	public MMWallet setTransferWallet(MMWallet mmw, List<MarketMakingWalletDto> mmwdNewLst, String secretKey) {
 		List<MarketMakingWalletDto> mmwdlLst = new ArrayList<MarketMakingWalletDto>();
-		mmwdlLst.addAll(mmwdNewLst.stream().map(mmwd -> fixMMWallet(mmwd, secretKey)).collect(Collectors.toList()));
+		mmwdlLst.addAll(mmwdNewLst.stream().map(mmwd -> {
+			mmwd = fixMMWallet(mmwd, secretKey);
+			mmwd.setCoinId(mmw.getCoinId());
+			mmwd.setBlockchain(mmw.getBlockchain());
+			mmwd.setCoin(mmw.getCoin());
+			mmwd.setCoinSymbol(mmw.getCoinSymbol());
+			mmwd.setBlockchainId(mmw.getBlockchainId());
+			mmwd.setContractId(mmw.getContractId());
+			mmwd.setWalletType(WalletType.TRANSFER);
+			return mmwd;
+		}).collect(Collectors.toList()));
 		mmw.setTransferWalletList(mmwdlLst);
 		return mmw;
 	}
 
-	public MMWallet setDefiWallet(MMWallet mmw, List<MarketMakingWalletDto> mmwdNewLst,
-			String secretKey) {
+	public MMWallet setDefiWallet(MMWallet mmw, List<MarketMakingWalletDto> mmwdNewLst, String secretKey) {
 		List<MarketMakingWalletDto> mmwdlLst = new ArrayList<MarketMakingWalletDto>();
-		mmwdlLst.addAll(mmwdNewLst.stream().map(mmwd -> fixMMWallet(mmwd, secretKey)).collect(Collectors.toList()));
-		mmw.setDefiWalletList(mmwdlLst);
+		mmwdlLst.addAll(mmwdNewLst.stream().map(mmwd -> {
+			mmwd = fixMMWallet(mmwd, secretKey);
+			mmwd.setCoinId(mmw.getCoinId());
+			mmwd.setBlockchain(mmw.getBlockchain());
+			mmwd.setCoin(mmw.getCoin());
+			mmwd.setCoinSymbol(mmw.getCoinSymbol());
+			mmwd.setBlockchainId(mmw.getBlockchainId());
+			mmwd.setContractId(mmw.getContractId());
+			mmwd.setWalletType(WalletType.TRANSFER);
+			return mmwd;
+		}).collect(Collectors.toList()));
+		mmw.setTransferWalletList(mmwdlLst);
 		return mmw;
 	}
 
@@ -57,40 +76,35 @@ public class MMWalletService {
 	}
 
 	public List<MMWallet> saveWithChunk(MMWallet mmw, int chunkSize, int defiWalletSize) {
-        List<MarketMakingWalletDto> transferWalletList = mmw.getTransferWalletList();
-        int chunkCount = (int) Math.ceil((double) transferWalletList.size() / chunkSize);
+		List<MarketMakingWalletDto> transferWalletList = mmw.getTransferWalletList();
+		int chunkCount = (int) Math.ceil((double) transferWalletList.size() / chunkSize);
 
-        List<MMWallet> chunkedData = IntStream.range(0, chunkCount)
-                .mapToObj(i -> {
-                    List<MarketMakingWalletDto> chunkedTransferList = transferWalletList.stream()
-                            .skip(i * chunkSize)
-                            .limit(chunkSize)
-                            .collect(Collectors.toList());
+		List<MMWallet> chunkedData = IntStream.range(0, chunkCount).mapToObj(i -> {
+			List<MarketMakingWalletDto> chunkedTransferList = transferWalletList.stream().skip(i * chunkSize)
+					.limit(chunkSize).collect(Collectors.toList());
 
-                    List<MarketMakingWalletDto> shuffledDefiWalletList = new ArrayList<>(mmw.getTransferWalletList());
-                    Collections.shuffle(shuffledDefiWalletList);
+			List<MarketMakingWalletDto> shuffledDefiWalletList = new ArrayList<>(mmw.getTransferWalletList());
+			Collections.shuffle(shuffledDefiWalletList);
 
-                    MMWallet mv = new MMWallet(mmw);
-                    mv.setChunk(i);
-                    mv.setTransferWalletList(chunkedTransferList);
+			MMWallet mv = new MMWallet(mmw);
+			mv.setChunk(i);
+			mv.setTransferWalletList(chunkedTransferList);
 
-                    if (i == 0) {
-                        mv.setDefiWalletList(shuffledDefiWalletList.stream()
-                                .limit(defiWalletSize)
-                                .collect(Collectors.toList()));
-                    }
+			if (i == 0) {
+				mv.setDefiWalletList(
+						shuffledDefiWalletList.stream().limit(defiWalletSize).collect(Collectors.toList()));
+			}
 
-                    return mv;
-                })
-                .map(mmWalletMicroService::save)
-                .collect(Collectors.toList());
+			return mv;
+		}).map(mmWalletMicroService::save).collect(Collectors.toList());
 
-        return chunkedData;
-    }
+		return chunkedData;
+	}
 
-	public List<MMWallet> saveWithChunk(MMWallet mmw, List<MarketMakingWalletDto> transferWalletList,int chunkSize, int defiWalletSize, String secretKey) {
+	public List<MMWallet> saveWithChunk(MMWallet mmw, List<MarketMakingWalletDto> transferWalletList, int chunkSize,
+			int defiWalletSize, String secretKey) {
 		mmw = setTransferWallet(mmw, transferWalletList, secretKey);
-        return saveWithChunk(mmw, chunkSize, defiWalletSize);
-    }
+		return saveWithChunk(mmw, chunkSize, defiWalletSize);
+	}
 
 }
