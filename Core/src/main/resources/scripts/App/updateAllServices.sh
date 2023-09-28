face1ea3f46d41f50cae83ccb661772f5bf88783
git <eae966@gmail.com>
2023-09-28 18:07:54 +0330

    Some Improvements.

--- Core/src/main/resources/scripts/App/updateAllServices.sh
+++ Core/src/main/resources/scripts/App/updateAllServices.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+NexiV1IP=10.20.6.20
+ssh root@$NexiV1IP "systemctl stop nexiDefi.service"
 \cp  /root/.m2/repository/com/plgchain/app/Cloud/0.0.1/Cloud-0.0.1.jar /root/PlingaHelperApp/
 \cp  /root/.m2/repository/com/plgchain/app/BlockchainHealthBusiness/0.0.1/BlockchainHealthBusiness-0.0.1.jar /root/PlingaHelperApp/
 \cp  /root/.m2/repository/com/plgchain/app/Transfer/0.0.1/Transfer-0.0.1.jar /root/PlingaHelperApp/
@@ -9,6 +11,8 @@
 rm -rf /var/log/plingaHelper/*
 rm -rf /home/kafka/data/*
 rm -rf /home/zookeeper/data/*
+ssh root@$NexiV1IP "rm -rf /var/log/plingaHelper/*"
+scp -r /root/.m2/repository/com/plgchain/app/NexiDefi/0.0.1/NexiDefi-0.0.1.jar root@$NexiV1IP:/root/PlingaHelperApp/
 /usr/bin/systemctl restart zookeeper.service
 /usr/bin/systemctl restart kafka.service
 /usr/bin/systemctl restart cloud.service
@@ -16,4 +20,5 @@ rm -rf /home/zookeeper/data/*
 /usr/bin/systemctl restart transfer.service
 /usr/bin/systemctl restart defiMarketMaking.service
 /usr/bin/systemctl restart ucenterApi.service
+ssh root@$NexiV1IP "systemctl restart nexiDefi.service"
 #/usr/bin/systemctl restart webapp.service
